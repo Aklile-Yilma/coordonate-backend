d6d765fa4c6e97dbfcf9e4d9e657dbded168714d
aklile <aklileyilma@gmail.com>
2023-05-03 21:34:49 +0300

    add jest, nodemon, dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
-
-let x = 1;
+console.log();
 
 app.listen('5000', () => {
   console.log('Server Running at port 5000');
